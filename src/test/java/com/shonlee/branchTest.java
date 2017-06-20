dc2d85bd142452efbc38417138872f292df6430d
xiao <xiao_7061@163.com>
2017-06-20 14:13:28 +0800

    add branchTest.java

---
+++ src/test/java/com/shonlee/branchTest.java
@@ -0,0 +1,10 @@
+package com.shonlee;
+
+/**
+ * Created by ShonLee on 2017/6/20.
+ */
+public class branchTest {
+    public static void main(String[] args) {
+        System.out.println("hello branchTest");
+    }
+}
